aa473ce06413cbcf34ab03e2696a087ff6f3b47a
mmgout04 <mgout@calpoly.edu>
2024-10-09 14:43:04 -0700

    Added users with post

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -44,13 +44,25 @@ const findUserByName = (name) => {
 const findUserById = (id) => 
   users["users_list"].find((user) => user["id"] === id);
 
-app.get("/users/:id", (req, res) => {
-  const id = req.params["id"];
-  let result = findUserById(id);
-  if (result === undefined) {
-    res.status(404).send("Resource not found.");
-  } else {
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
+
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
     res.send(result);
+  } else {
+    res.send(users);
   }
 });
 
